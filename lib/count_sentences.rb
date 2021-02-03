0fe703c0112e161b0f636ac42350a130668ed705
Robert Hardy <rwhj77@hotmail.com>
2021-02-03 17:24:03 -0600

    commit tree 8c24cb73a6388977246ec7fed36d634bd859e54f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.match(/\w+\.\z/).nil? ? false : true
   end
 
   def question?
-
+    self.match(/\w+\?\z/).nil? ? false : true
   end
 
   def exclamation?
-
+    self.match(/\w+!\z/).nil? ? false : true
   end
 
   def count_sentences
-
+    self.scan(/\w+[.!?]{1,}/).length
   end
 end
